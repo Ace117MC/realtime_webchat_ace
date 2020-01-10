6f2cadcddcfd515b345b5ed1b5b7a1c229a05077
Anmol Bansal <bansal.anmol1999@gmail.com>
2020-01-11 01:50:36 +0530

    sequelize change for postgres
    Signed-off-by: Anmol Bansal <bansal.anmol1999@gmail.com>

--- db/model.js
+++ db/model.js
@@ -2,7 +2,13 @@ const Sequelize = require('sequelize')
 let db
 
 if(process.env.DATABASE_URL) {
-    db = new Sequelize(process.env.DATABASE_URL)
+    db = new Sequelize(process.env.DATABASE_URL, {
+        dialect:  'postgres',
+        protocol: 'postgres',
+        port:     match[4],
+        host:     match[3],
+        logging:  true
+    })
 } else {
     db = new Sequelize({
         dialect: 'sqlite',
